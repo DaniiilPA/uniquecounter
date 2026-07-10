4173ba759c240e488f0fab2272986a842178caaf
DaniiilPA <pereslegindanya@gmail.com>
2026-07-10 06:17:51 +0300

    first

---
+++ PluginCore.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using ExileCore;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
+using ExileCore.Shared.Enums;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using System.Numerics;
+
+namespace UniqueLogger
+{
+    public class UniqueLoggerSettings : ISettings
+    {
+        public ToggleNode Enable { get; set; } = new ToggleNode(true);
+    }
+
+    public class UniqueLogger : BaseSettingsPlugin<UniqueLoggerSettings>
+    {
+        public override bool Initialise()
+        {
+            return true;
+        }
+
+        public override void Render()
+        {
+            if (!Settings.Enable) return;
+
+            var uniqueItems = new List<string>();
+
+            var entities = GameController?.EntityListWrapper?.Entities;
+            if (entities == null) return;
+
+            foreach (var entity in entities)
+            {
+                if (entity == null || !entity.IsValid) continue;
+
+                var worldItem = entity.GetComponent<WorldItem>();
+                if (worldItem == null) continue;
+
+                var itemEntity = worldItem.ItemEntity;
+                if (itemEntity == null || !itemEntity.IsValid) continue;
+
+                var mods = itemEntity.GetComponent<Mods>();
+                if (mods == null || mods.ItemRarity != ItemRarity.Unique) continue;
+
+                var baseItemType = itemEntity.GetComponent<Base>()?.Name ?? "Unknown Base";
+
+                var renderItem = itemEntity.GetComponent<RenderItem>();
+                string uniqueName = "Unidentified";
+
+                if (renderItem != null && !string.IsNullOrEmpty(renderItem.ResourcePath))
+                {
+                    var path = renderItem.ResourcePath;
+                    var fileName = Path.GetFileNameWithoutExtension(path);
+                    if (!string.IsNullOrEmpty(fileName))
+                    {
+                        uniqueName = fileName;
+                    }
+                }
+
+                uniqueItems.Add($"{baseItemType} ({uniqueName})");
+            }
+
+            var windowRect = GameController?.Window?.GetWindowRectangleTimeCache;
+            if (windowRect != null && uniqueItems.Count > 0)
+            {
+                var drawPos = new Vector2(windowRect.Value.Width * 0.15f, windowRect.Value.Height * 0.15f);
+                var yOffset = 0f;
+
+                foreach (var itemText in uniqueItems.Distinct())
+                {
+                    Graphics.DrawText(itemText, drawPos + new Vector2(0, yOffset), Color.White, 15);
+                    yOffset += 18f; 
+                }
+            }
+        }
+    }
+}
